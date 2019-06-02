d58c2ae9049970a4efe1959c1a15a8f0f6d751e9
jose <jmvr37@hotmail.com>
2019-06-01 17:01:15 -0700

    grid added

--- js/script.js
+++ js/script.js
@@ -8,19 +8,33 @@ $("select").on("change", function() {
       section +
       ".json?api-key=MCJhGoJthqCkQuDvJvgX3isw1LxKIpgO"
   }).done(function(data) {
-    // console.log("data", data.results[0]);
+    let cont = 0;
+    // console.log("data", data.results.title);
+    $(".home").html("");
     $.each(data.results, function(key, value) {
-      // console.log(data.results[key].url);
-      try {
-        $(".home").append(
-          `<li><img src='${data.results[key].multimedia[2].url}'></li>
-          <p>'${data.results[key].title}'</p><a> href="'${
-            data.results[key].url
-          }'"</a>`
+      // console.log(data.results[key].abstract);
+      if (data.results[key].multimedia.length >= 5 && cont < 12) {
+        try {
+          cont = cont + 1;
+          $(".home").append(
+            `<li class="style">
+            <a href='${data.results[key].url}'>
+              <div class=image style="background-image: url('${
+                data.results[key].multimedia[4].url
+              }')">
+                <div>
+                  <p class="para">'${data.results[key].abstract}'</p>
+                  </div>
+                        </div>
+                          </a>
+                            </li>`
 
-          // `<ul>'${data.results[1].title}'</ul>`
-        );
-      } catch {}
+            // `<ul>'${data.results[1].title}'</ul>`
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      }
     });
   });
 });
